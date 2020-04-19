071fbf21e495f37265b73f5e788bc5977e9d360a
caijw <285025525@qq.com>
2020-04-20 01:03:47 +0800

    feat: update

--- listing_2.4.cpp
+++ listing_2.4.cpp
@@ -1,12 +1,17 @@
 #include <thread>
 #include <string>
+#include <iostream>
 
 void open_document_and_display_gui(std::string const& filename)
-{}
+{
+    std::cout << "open_document_and_display_gui" << std::endl;
+}
 
 bool done_editing()
 {
+    std::cout << "done_editing" << std::endl;
     return true;
+    // return false;
 }
 
 enum command_type{
@@ -25,19 +30,24 @@ struct user_command
 
 user_command get_user_input()
 {
+    std::cout << "get_user_input" << std::endl;
     return user_command();
 }
 
 std::string get_filename_from_user()
 {
+    std::cout << "get_filename_from_user" << std::endl;
     return "foo.doc";
 }
 
 void process_user_input(user_command const& cmd)
-{}
+{
+    std::cout << "process_user_input" << std::endl;
+}
 
 void edit_document(std::string const& filename)
 {
+    std::cout << "edit_document" << std::endl;
     open_document_and_display_gui(filename);
     while(!done_editing())
     {
